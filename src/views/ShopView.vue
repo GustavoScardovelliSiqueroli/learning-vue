7bcaa2f0295e4efa7e8dfd2199b404722f9f7ba4
gustavo <gustavosecardovelli@gmail.com>
2025-09-19 17:27:27 -0300

    feat: item removed to shop on buy

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="store-container">
-    <StoreWindow
-      :items="[
-        {
-          name: 'auto-clicker',
-          title: 'Auto Clicker',
-          value: 20,
-          amount: 1,
-          currency: 'black-coin',
-        },
-        {
-          name: 'click-value',
-          title: 'Increase click value $$$',
-          message: 'Increase by 1',
-          value: 50,
-          amount: 1,
-          currency: 'coin',
-        },
-      ]"
-      @buy-item="buyItem"
-    ></StoreWindow>
+    <StoreWindow :items="items" @buy-item="buyItem"></StoreWindow>
   </div>
 </template>
 
 <script setup lang="ts">
 import StoreWindow from '@/components/ShopWindow.vue'
 import type { ShopItem } from '@/types/shop-item'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+
+const items: Ref<ShopItem[]> = ref([
+  {
+    name: 'auto-clicker',
+    title: 'Auto Clicker',
+    value: 20,
+    amount: 1,
+    currency: 'black-coin',
+  },
+  {
+    name: 'click-value',
+    title: 'Increase click value $$$',
+    message: 'Increase by 1',
+    value: 50,
+    amount: 2,
+    currency: 'coin',
+  },
+])
 
 function buyItem(item: ShopItem) {
-  console.log(item)
+  if (item.name == 'click-value') {
+    if (item.amount > 1) {
+      item.amount--
+      return
+    }
+    items.value = items.value.filter((aItem) => aItem.name != 'click-value')
+  }
 }
 </script>
 
