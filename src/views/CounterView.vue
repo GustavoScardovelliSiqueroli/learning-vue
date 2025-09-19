c8194919f73ef59dd770baac1d7970dc64a6bff7
gustavo <gustavosecardovelli@gmail.com>
2025-09-19 11:04:23 -0300

    feat: button effect

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="counter-container">
-    <button @click="incrementCounterMoney" style="height: 50px; width: 80px">
-      Somar 1
-    </button>
+    <button
+      @click="incrementCounterMoney"
+      style="height: 50px; width: 80px"
+    ></button>
     <h1>{{ counter.count }} / {{ counter.goal }}</h1>
     <h1 :style="`opacity: ${counter.isAchieved ? 100 : 0};`">🎉</h1>
   </div>
@@ -17,7 +18,7 @@ function incrementCounterMoney() {
 }
 </script>
 
-<style>
+<style scoped>
 .counter-container {
   display: flex;
   width: 100%;
@@ -25,5 +26,20 @@ function incrementCounterMoney() {
   justify-content: center;
   align-items: center;
   gap: 30px;
+}
+button {
+  background-color: #60686c;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+  border: none;
+  transition: all 0.05s;
+}
+button:hover {
+  background-color: #505659;
+}
+button:active {
+  background-color: #505659;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
 }
 </style>
